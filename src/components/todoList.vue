484b65675c9300ca8d5290f7b480503f361153e3
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-15 10:07:58 -0300

    mudando o titulo e o campo input

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,8 +1,8 @@
 <template>
      <div class="conteiner">
-    <h3>Digite um item</h3>
-    <button>status</button>
-    <input type="text" v-model.trim="lista.text" placeholder="Digite Qualque coisa"> <!-- ok-->
+    <h1>Bem-Vindo ao Gerenciamento de Tarefas</h1>
+    <button>Status</button>
+    <input type="text" v-model.trim="lista.text" placeholder="Digite Uma Tarefa aqui"> 
     <button type="button" @click="addLista()" > Enviar</button>
 
     <div v-for="(lista, index) in listas" :key="index" >
