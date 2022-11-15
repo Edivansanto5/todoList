e3191c87abd021541cd244a27a3b1d052974eaf8
EdivanSanto5 <capic20191tads0326@aluno.ifpi.edu.br>
2022-11-15 08:58:14 -0300

    removendo compomnente que nao precisa

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
-
-    <!-- <toList/>  -->
      <todoList/> 
-
-    
   </div>
 </template>
 <script>
 import todoList from './components/todoList.vue'
-//import toList from './components/toList.vue'
 
   export default{
     components:{
-     // toList,
       todoList
     }
   }
--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -6,25 +6,10 @@
 
     <div v-for="(lista, index) in listas" :key="index" >
         <span>{{ lista.text }}</span> <a class="mr-10" href="#" @click="deleta">Excluir</a>
-        <span>{{listas.text}}</span>
+        
     </div>
     <br>
   </div>
-    <!-- <div class="conteiner">
-      <div class="box">
-            <h1>{{title}}</h1>
-        </div>
-        <h1>{{nome}}</h1>
-        <button class="btn" type="###">Status</button>
-        <input type="text" v-model="lista.text" placeholder="Digite aqui uma Tarefa"> 
-        <button class="btn" @click="addLista()">Adcionar</button>
-
-        <div v-for="(lista,index) in listas" :key="index">
-           <p>fasdfadfaf</p>
-        </div>
-        
-   
-    </div> -->
 </template>
 <script>
 
